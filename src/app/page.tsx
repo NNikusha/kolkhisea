3051b5d13f08b9072eea34a6c1233c94076ae0f2
NNikusha <116583730+NNikusha@users.noreply.github.com>
2025-03-19 17:43:45 +0400

    installation

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="container flex flex-col justify-center px-4 mx-auto h-full">
+      mobrdzandi
+
+    </div>
+  )
+}
+
+export default page
