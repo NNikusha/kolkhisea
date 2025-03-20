86d2bf23774efd07fe9b1b382cf471c9ff0b2101
NNikusha <116583730+NNikusha@users.noreply.github.com>
2025-03-20 18:12:02 +0400

    error update

--- src/app/components/atoms/Badge/Badge.tsx
+++ src/app/components/atoms/Badge/Badge.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Badge = ({ text }:any) => (
-  <div className="px-4 py-2 rounded-full font-medium text-sm w-fit text-[#285260] bg-[#285260]/5">
+interface BadgeProps {
+  text: string;
+  className?: string;
+}
+
+const Badge: React.FC<BadgeProps> = ({ text, className = '' }) => (
+  <div className={`px-4 py-2 rounded-full font-medium text-sm w-fit text-[#285260] bg-[#285260]/5 ${className}`}>
     {text}
   </div>
 );
--- src/app/components/atoms/Button/Button.tsx
+++ src/app/components/atoms/Button/Button.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import DiagonalArrow from '@/app/assets/DiagonalArrow';
 
-const Button = ({ text }:any) => (
-  <button className="bg-[#CB684D] text-white py-4 px-6 rounded-[16px] flex items-center gap-2">
-    {text}
-    <div className='flex w-[15px] h-[15px]'>
-      <DiagonalArrow />
-    </div>
-  </button>
+interface ButtonProps {
+    text: string;
+    onClick?: () => void;
+    className?: string;
+    fullWidth?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    text,
+    onClick,
+    className = '',
+    fullWidth = false
+}) => (
+    <button
+        onClick={onClick}
+        className={`  bg-[#CB684D] text-white py-4 px-6 rounded-[16px] flex items-center gap-2 ${fullWidth ? 'w-full justify-center' : ''}${className}
+    `}
+    >
+        {text}
+        <div className='flex w-[15px] h-[15px]'>
+            <DiagonalArrow />
+        </div>
+    </button>
 );
 
 export default Button;
