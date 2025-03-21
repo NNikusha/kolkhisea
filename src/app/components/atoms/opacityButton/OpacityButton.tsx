3fd5eaa0bfb0b9e50d3c3fd923592cd5797abd2d
Bachidze <bachidze90@gmail.com>
2025-03-22 01:13:52 +0400

    feat(fix):Conflict

--- src/app/components/atoms/opacityButton/OpacityButton.tsx
+++ src/app/components/atoms/opacityButton/OpacityButton.tsx
@@ -1,45 +1,31 @@
-
-import Image from "next/image";
-import Link from "next/link";
-import ArrowRight from "@/app/assets/arrow-up-right.svg";
-=======
+import React from "react";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
+import ArrowRight from "@/app/assets/arrow-up-right.svg";
 
-import React from "react";
-
-interface OpacityButtonProp {
+interface OpacityButtonProps {
   text: string;
-
-}
-export default function OpacityButton({ text }: OpacityButtonProp) {
-  return (
-    <div
-      className="mt-14 flex items-center gap-3.5 bg-white/15 shadow-inner backdrop-blur-[8.6px] w-[203px] py-4 px-[24px] rounded-2xl cursor-pointer hover:bg-white/30 transition-all duration-500"
-=======
   className?: string;
   image?: StaticImageData | string;
 }
+
 export default function OpacityButton({
   text,
   className,
   image,
-}: OpacityButtonProp) {
+}: OpacityButtonProps) {
   return (
     <div
-      className={`mt-14 flex items-center gap-3.5 shadow-inner backdrop-blur-[8.6px]  cursor-pointer transition-all duration-500 ${
-        className || " py-4 px-[24px]  rounded-2xl w-[203px] bg-white/15 hover:bg-white/30"
+      className={`mt-14 flex items-center gap-3.5 shadow-inner backdrop-blur-[8.6px] cursor-pointer transition-all duration-500 ${
+        className || "py-4 px-[24px] rounded-2xl w-[203px] bg-white/15 hover:bg-white/30"
       }`}
-
       style={{ boxShadow: "0px 4px 24.9px 0px #FFFFFF80 inset" }}
     >
       <Link href={"/"}>
         <button className="cursor-pointer">{text}</button>
       </Link>
 
-      <Image alt="InsideView" src={ArrowRight} width={15} height={15} />
-
-      {image && <Image alt="InsideView" src={image} width={15} height={15} />}
+      {image && <Image alt="Custom Icon" src={image} width={15} height={15} />}
     </div>
   );
 }
