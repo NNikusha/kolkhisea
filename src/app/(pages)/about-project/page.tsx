8716b80f222c709794d15f15fc15ce7b98168d51
guramius <96048349+guramius@users.noreply.github.com>
2025-03-23 14:55:52 +0400

    working WhoWeAre
    add texts and image

--- src/app/(pages)/about-project/page.tsx
+++ src/app/(pages)/about-project/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AboutProject from '@/app/components/organisms/AboutProject/AboutProject';
+import WhoWeAre from '@/app/components/atoms/WhoWeAre/WhoWeAre';
 
 const page = () => {
     return (
      <>
-     <AboutProject/>
+        <AboutProject/>
+        <WhoWeAre />
      </>
     );
 };
---
+++ src/app/components/atoms/WhoWeAre/WhoWeAre.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Image from 'next/image'
+import faceIcone from '@/app/assets/faceIcone.svg'
+import mainBlock from '@/app/assets/mainBlock.svg'
+import vector from "@/app/assets/Vector.svg";
+const WhoWeAre = () => {
+  return (
+    <div className='relative px-[300px] bg-[##F3F6FB]'>
+        <Image  
+            alt="Background pattern"
+            layout="fill"
+            objectFit="center"
+            priority 
+            src={vector} 
+        />
+        <div className='flex justify-between pb-[48px]'>
+            <div>
+                <h1 className='text-[#1C1C1E] text-[64px] pb-[48px]'>Who <span className='text-[#ABABAB]'>We Are</span> </h1>
+                <p className='text-[#3D3D3D]'>With years of experience in the real estate and construction industry, [Company Name] has become a trusted name in delivering high-end residential and commercial projects. Our team of skilled architects, engineers, and designers work together to craft spaces that are not only aesthetically pleasing but also functional and sustainable.</p>
+            </div>
+            <div>
+                <div className='flex item-centre'>
+                    <p className='pb-[48px] text-[64px] text-[#000000]'>28+</p>
+                    <Image src= {faceIcone} objectFit='' layout='' width={224} height={80} alt='faceIcone'/>
+                </div>
+                <p className='text-[#3D3D3D]'>Our team consists of experienced architects, engineers, and real estate professionals who are passionate about delivering exceptional living and investment experiences. We collaborate with top industry experts to ensure our projects meet the highest standards.</p>
+            </div>
+        </div>
+        <div>
+            <Image src= {mainBlock} className="w-full relative" objectFit='cover' layout='responsive' width={900} height={80} alt='faceIcone'/>
+        </div>
+    </div>
+  )
+}
+
+export default WhoWeAre
