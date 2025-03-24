898290b0495085e42a3a71e40eaffa2710e6ee24
NNikusha <116583730+NNikusha@users.noreply.github.com>
2025-03-24 14:53:31 +0400

    Update page.tsx

--- src/app/(pages)/about-project/page.tsx
+++ src/app/(pages)/about-project/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import AboutProject from '@/app/components/organisms/AboutProject/AboutProject';
-import WhoWeAre from '@/app/components/atoms/WhoWeAre/WhoWeAre';
 
 const page = () => {
     return (
