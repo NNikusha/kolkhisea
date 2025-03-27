7cc5785c272a1b4a6bb60655431defaf7f0807b8
guramius <96048349+guramius@users.noreply.github.com>
2025-03-27 20:35:14 +0400

    i hope its done

--- src/app/components/atoms/TheJourney/TheJourney.tsx
+++ src/app/components/atoms/TheJourney/TheJourney.tsx
@@ -11,21 +11,23 @@ interface TheJourneyProps {
 
 const TheJourney = ({ imageBg, about, title, titleSpan, text }: TheJourneyProps) => {
     return (
-        <div className='relative h-[581px] bg-[#F3F6FB] rounded-[25px] text-[#3D3D3D]'>
-            <Image src={imageBg} width={480} height={400} alt='TheJourneyBg' objectFit='cover' />
-            <div className=''>
-                <div className='absolute w-[480px] h-[100px] top-[210px] bg-gradient-to-t from-[#F3F6FB] via-[#DEE6ED] to-transparent'>
-                    <p className='inline ml-[32px] rounded-[10px] px-[16px] py-[16.5px] bg-white text-[#285260]'>
+        <div className='relative h-[497px] lg:h-[581px] bg-[#F3F6FB] rounded-[25px] text-[#3D3D3D]'>
+            <div>
+             <Image className='w-[343px] lg:w-[350px] xl:w-[480px]' src={imageBg} width={480} height={400} alt='TheJourneyBg' objectFit='responsive' />  
+             <div className='absolute w-[100%] h-[90px] top-[25%] xl:top-[36%] bg-gradient-to-t from-[#F3F6FB] via-[#DEE6ED] to-transparent'>
+                    <p className='inline ml-[16px] lg:ml-[32px] rounded-[200px] px-[16px] py-[10px] lg:py-[16.5px] bg-white text-[#285260]'>
                         {about}
                     </p>
-                    <h3 className='pl-[32px] pt-[16px] pb-[24px] text-2xl text-[#1c1c1e]'>
+                    <h3 className='pl-[16px] lg:pl-[32px] pt-[16px] pb-[24px] text-[18px] lg:text-[24px] text-[#1c1c1e]'>
                         {title}
                         <span className='text-[#cb684d]'>
                             {titleSpan}
                         </span>
                     </h3>
-                </div>
-                <p className='pt-[24px] pb-[40px] px-[32px] w-[480px] '>
+                </div>             
+            </div>
+            <div>
+                <p className='lg:pt-[24px] lg:pb-[40px] lg:px-[32px] pt-[16px] pb-[24px] px-[16px] lg:text-[16px] text-[14px] w-[311px] lg:w-[350px] xl:w-[480px] '>
                     {text}
                 </p>
             </div>
--- src/app/components/molecules/TheJourneyMerg/TheJourneyMerg.tsx
+++ src/app/components/molecules/TheJourneyMerg/TheJourneyMerg.tsx
@@ -5,19 +5,22 @@ import TheJourneyBg2 from '@/app/assets/TheJourneyBg2.svg'
 
 const TheJourneyMerg = () => {
     return (
-        <div className='relative container lg:px-[150px] mx-auto w-full'>
+        <div className='relative container lg:px-[150px] mx-auto w-full mb-[200px]'>
             <div>
-                <h1 className='flex justify-between items-center text-4xl text-[#B4B4B4]'>
+                <h1 className='flex flex-col lg:flex-row uppercase justify-between items-center lg:text-4xl text-[24px] text-[#B4B4B4]'>
                     <span>The Journey</span>
-                    <span className='pt-[15px]'>                    
-                        <svg width="424" height="1" viewBox="0 0 424 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <span className='pt-[15px] pb-[15px] lg:pt-[0] lg:pb-[0]'>                    
+                        <svg className='hidden lg:block w-[150px] xl:w-[424px]'  height="5" viewBox="0 0 424 1" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <line y1="0.5" x2="424" y2="0.5" stroke="#C2C2C2"/>
+                        </svg>
+                        <svg className="block lg:hidden"  width="89" height="1" viewBox="0 0 89 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <line opacity="0.5" x1="0.5" y1="0.5" x2="88.5" y2="0.500008" stroke="#C2C2C2"/>
                         </svg>
                     </span>
                     <span className='text-[#1C1C1E]'>of Creation</span>
                 </h1> 
             </div>
-            <div className='flex justify-between pt-[56px]'>
+            <div className='flex flex-col gap-[24px] lg:flex-row justify-between items-center pt-[56px]'>
                 <TheJourney 
                     imageBg={TheJourneyBg}
                     about= 'Before'
