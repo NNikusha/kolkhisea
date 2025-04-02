b97105143278fb01b3ca95c69400158dcaf87f63
zikniki <102323125+zikniki@users.noreply.github.com>
2025-04-03 01:31:40 +0400

    KOL-19/update
    adding flat-detail-page.
    adding ApartmentInformation section, desktop and mobile.
    fixing FinishingApartment border-b and overlay color bug for about-project and flat-detail-page.

--- src/app/components/organisms/FlatDetailPage/FlatDetailPage.tsx
+++ src/app/components/organisms/FlatDetailPage/FlatDetailPage.tsx
@@ -1,19 +1,24 @@
+'use client'
+
 import React from 'react'
 import FinishingApartment from '../../atoms/FinishingApartment/FinishingApartment';
 import SuggestApartment from '../../atoms/SuggestApartment/SuggestApartment';
 import FlatDetailPageFeatureCards from '../../atoms/FlatDetailPageFeatureCards/FlatDetailPageFeatureCards';
+import ApartmentInformation from '../../molecules/ApartmentInformation/ApartmentInformation';
+import ApartmentInformationCard from '../../atoms/ApartmentInformationCard/ApartmentInformationCard';
 
 
 const FlatDetailPage = () => {
   return (
     <div className='w-full bg-[#F3F6FB]'>
-        <div className='container mx-auto min-h-[500px] px-4 lg:px-[108px]'>
-            <p className='text-black'>place for component</p>
-        </div>
-        <div className='w-full bg-white rounded-t-[56px] mt-[100px] pt-[24px] lg:pt-[60px] pb-[72px] md:pb-[168px]'>
+        <ApartmentInformation/>
+        <div className='w-full bg-white rounded-t-[56px] mt-[32px] lg:mt-[40px] pt-[24px] lg:pt-[60px] pb-[72px] md:pb-[168px]'>
             <div className="container mx-auto">
+                <div className='flex lg:hidden'>
+                  <ApartmentInformationCard/>
+                </div>
                 <FlatDetailPageFeatureCards/>
-                <FinishingApartment/>
+                <FinishingApartment gradientColor="#FFFFFF" />
                 <SuggestApartment/>
             </div>
         </div>
