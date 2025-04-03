f3d83b8d615a4bdd9adb148fbde562a38b0b7f13
zikniki <102323125+zikniki@users.noreply.github.com>
2025-04-04 02:01:12 +0400

    KOL-24
    adding contact page buttons.
    fixing contact page padding-x.

--- src/app/components/organisms/AboutContact/AboutContact.tsx
+++ src/app/components/organisms/AboutContact/AboutContact.tsx
@@ -17,11 +17,46 @@ export default function AboutContact() {
   return (
     <>
       <Header mainImage={MainApartment} mobileMainImage={MobileMainApartment} />
-      <section className="container px-[20px] lg:px-[150px] mx-auto mt-[146px] xl:mt-[200px]">
-        <div className="relative mt-[25%] flex flex-col justify-center items-center">
-          <div className="hidden md:absolute" style={{ zIndex: -1 }}>
+      <section className="container px-[16px] lg:px-[108px] mx-auto mt-[146px] xl:mt-[200px]">
+        <div className="relative lg:mt-[40%] xl:mt-[30%] 2xl:mt-[25%] flex flex-col justify-center items-center">
+          <div className="hidden lg:block absolute lg:bottom-[-100px] xl:bottom-[25px] 2xl:bottom-[-80px]" style={{ zIndex: 20 }}>
             <Elipse />
           </div>
+            <button
+              className="absolute cursor-pointer hidden lg:block absolute uppercase leading-[1.3] text-[16px] text-white py-[24px] px-[16px] rounded-[16px] bg-[#F4EDE633]/20 z-21 backdrop-blur-[8.6px] lg:left-[-70px] lg:bottom-[-150px] xl:left-[30px] xl:bottom-[-40px] 2xl:left-[150px] 2xl:bottom-[-100px]"
+              style={{
+                boxShadow: "inset 0 0 25px rgba(255, 255, 255, 0.3)",
+              }}
+            >
+              What materials do you use?
+            </button>
+
+            <button
+              className="absolute cursor-pointer hidden lg:block absolute uppercase leading-[1.3] text-[16px] text-white py-[24px] px-[16px] rounded-[16px] bg-[#F4EDE633]/20 z-21 backdrop-blur-[8.6px] lg:left-[-90px] lg:bottom-[400px] xl:left-[0px] xl:bottom-[510px] 2xl:left-[10px] 2xl:bottom-[410px]"
+              style={{
+                boxShadow: "inset 0 0 25px rgba(255, 255, 255, 0.3)",
+              }}
+            >
+              Do you work on residential or commercial buildings?
+            </button>
+
+            <button
+              className="absolute cursor-pointer hidden lg:block absolute uppercase leading-[1.3] text-[16px] text-white py-[24px] px-[16px] rounded-[16px] bg-[#F4EDE633]/20 z-21 backdrop-blur-[8.6px] lg:right-[-90px] lg:bottom-[295px] xl:right-[0px] xl:bottom-[400px] 2xl:right-[70px] 2xl:bottom-[350px]"
+              style={{
+                boxShadow: "inset 0 0 25px rgba(255, 255, 255, 0.3)",
+              }}
+            >
+              How long does construction take?
+            </button>
+
+            <button
+              className="absolute cursor-pointer hidden lg:block absolute uppercase leading-[1.3] text-[16px] text-white py-[24px] px-[16px] rounded-[16px] bg-[#F4EDE633]/20 z-21 backdrop-blur-[8.6px] lg:right-[-70px] lg:bottom-[-70px] xl:right-[0px] xl:bottom-[0px] 2xl:right-[130px] 2xl:bottom-[-38px]"
+              style={{
+                boxShadow: "inset 0 0 25px rgba(255, 255, 255, 0.3)",
+              }}
+            >
+              What guarantees do you offer?
+            </button>
           <div className="flex justify-center w-full">
             <button className="bg-[#FFFFFF0D] text-[#FFFFFF] px-[16px] py-[16px] rounded-full mb-[40px]">
               Contact us
