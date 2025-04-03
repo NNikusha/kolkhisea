43bad90562c73b4d0f5e40fc220086ce04b3fb5d
NNikusha <116583730+NNikusha@users.noreply.github.com>
2025-04-03 19:49:08 +0400

    update

---
+++ src/app/components/molecules/GoogleMap/GoogleMap.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+
+const GoogleMap = () => {
+  return (
+    <div className="w-full mt-[72px] mb-12">
+        <div className="rounded-3xl overflow-hidden shadow-lg">
+          <iframe
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD1Ac6YQLrQGZzszYdEKVSgHyUiMWJVJB4&q=Lubliana+Street,Tbilisi,Georgia"
+            width="100%"
+            height="453"
+            style={{ border: 0 }}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            className="w-full h-[288px] md:h-[453px]"
+          ></iframe>
+        </div>
+    </div>
+  );
+};
+
+export default GoogleMap;
+
+
+
--- src/app/components/organisms/AboutContact/AboutContact.tsx
+++ src/app/components/organisms/AboutContact/AboutContact.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React from "react";
 import DownScroll from "@/app/assets/DownScroll.svg";
 import DownScrollArrow from "@/app/assets/DownScrollArrow.svg";
@@ -9,61 +8,55 @@ import MainHeadLine from "../../atoms/MainHeadLine/mainHeadLine";
 import DownScrollAnimation from "../../molecules/downScrollAnimation/DownScrollAnimation";
 import Header from "../../organisms/header/Header";
 import Elipse from "@/app/assets/Elipse";
-import faceIcon from "../../../assets/faceIcone.svg"
+import faceIcon from "../../../assets/faceIcone.svg";
 import Image from "next/image";
 import ContactUsMarge from "../../molecules/ContactUsMarge/ContactUsMarge";
+import GoogleMap from "../../molecules/GoogleMap/GoogleMap"; // Import the GoogleMap component
 
 export default function AboutContact() {
-    return (
-        <>
-            <Header
-                mainImage={MainApartment}
-                mobileMainImage={MobileMainApartment}
+  return (
+    <>
+      <Header mainImage={MainApartment} mobileMainImage={MobileMainApartment} />
+      <section className="container px-[20px] lg:px-[150px] mx-auto mt-[146px] xl:mt-[200px]">
+        <div className="relative mt-[25%] flex flex-col justify-center items-center">
+          <div className="hidden md:absolute" style={{ zIndex: -1 }}>
+            <Elipse />
+          </div>
+          <div className="flex justify-center w-full">
+            <button className="bg-[#FFFFFF0D] text-[#FFFFFF] px-[16px] py-[16px] rounded-full mb-[40px]">
+              Contact us
+            </button>
+          </div>
+          <MainHeadLine
+            className="text-center relative z-10"
+            width="max-w-[54%]"
+            centered={true}
+            secondText="If you have any Questions Contact US"
+            firstTextColor="#FFFFFF50"
+            firstText={""}
+            showTestSpan={false}
+          />
+          <div className="flex justify-start items-center gap-2 pb-[24px]">
+            <Image
+              className=""
+              src={faceIcon}
+              width={224}
+              height={80}
+              alt="faceIcone"
             />
-            <section className="container px-[20px] lg:px-[150px] mx-auto mt-[146px] xl:mt-[200px]">
-
-
-                <div className="relative mt-[25%] flex flex-col flex justify-center items-center">
-
-                    <div className=" hidden md:absolute" style={{ zIndex: -1 }}>
-                        <Elipse />
-                    </div>
-
-
-                    <div className="flex justify-center w-full">
-                        <button className=" bg-[#FFFFFF0D] text-[#FFFFFF] px-[16px] py-[16px] rounded-full mb-[40px]">
-                            Contact us
-                        </button>
-                    </div>
-                    <MainHeadLine
-                        className="text-center relative z-10"
-                        width="max-w-[54%]"
-                        centered={true}
-                        secondText="If you have any Questions Contact US"
-                        firstTextColor="#FFFFFF50"
-                        firstText={""}
-                        showTestSpan={false}
-                    />
-                    <div className='flex justify-start items-center gap-2 pb-[24px]'>
-                        <Image
-                            className=''
-                            src={faceIcon}
-                            width={224}
-                            height={80}
-                            alt='faceIcone'
-                        />
-                    </div>
-                </div>
-
-                <DownScrollAnimation
-                    DownScroll={DownScroll}
-                    DownScrollArrow={DownScrollArrow}
-                />
-                <div className=" mt-[346px] sm:mt-[600px] xl:mt-[300px] pt-[112px]">
-                    <ContactUsMarge />
-
-                </div>
-            </section>
-        </>
-    );
+          </div>
+        </div>
+        <DownScrollAnimation
+          DownScroll={DownScroll}
+          DownScrollArrow={DownScrollArrow}
+        />
+        
+        
+        <div className="mt-[350px] sm:mt-[600px] xl:mt-[300px] my-[200px] pt-[112px]">
+          <ContactUsMarge />
+        <GoogleMap />
+        </div>
+      </section>
+    </>
+  );
 }
