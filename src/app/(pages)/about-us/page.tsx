e45e1d3fbf74434dafd1452aa830dcf881c25cfe
NNikusha <116583730+NNikusha@users.noreply.github.com>
2025-04-10 19:25:04 +0400

    Create page.tsx

---
+++ src/app/(pages)/about-us/page.tsx
@@ -0,0 +1,13 @@
+
+import AboutUs from '@/app/components/organisms/AboutUs/AboutUs'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <AboutUs/>
+    </div>
+  )
+}
+
+export default page
