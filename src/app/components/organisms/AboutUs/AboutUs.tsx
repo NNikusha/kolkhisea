9859dcba581f3d18cb8ba452f472940554143aff
NNikusha <116583730+NNikusha@users.noreply.github.com>
2025-04-10 19:22:50 +0400

    Update AboutUs.tsx

--- src/app/components/organisms/AboutUs/AboutUs.tsx
+++ src/app/components/organisms/AboutUs/AboutUs.tsx
@@ -12,11 +12,12 @@ import BackGroundLine9 from '@/app/assets/BackGroundLine9'
 const AboutUs = () => {
     return (
         <div className="flex flex-col w-full">
-            <section className="relative h-[100vh]  md:h-[110vh] w-full  z-[-1]">
+            <section className="relative h-[100vh] md:h-[110vh] w-full z-[-1]">
                 <AboutUsSection />
             </section>
-            <div className='relative'>
-            <div className="absolute inset-0 w-full h-full overflow-hidden pointer-events-none">
+
+            <div className="relative">
+                <div className="absolute inset-0 w-full h-full overflow-hidden pointer-events-none">
                     <BackGroundLine6 className="absolute bottom-[0%] w-full h-auto max-w-none z-[-1]" />
                     <BackGroundLine1 className="absolute top-[10%] w-full h-auto max-w-none z-[0]" />
                 </div>
@@ -24,15 +25,14 @@ const AboutUs = () => {
                 <WhoWeAre />
                 <ChooseUsSection />
             </div>
-            <div className="w-full bg-white relative rounded-t-[56px] lg:py-[5px] 2xl:py-[80px] mt-[72px] 2xl:mt-[168px] relative">
+
+            <div className="w-full bg-white relative rounded-t-[56px] lg:py-[5px] 2xl:py-[80px] mt-[72px] 2xl:mt-[168px]">
                 <BackGroundLine9 className="absolute 2xl:bottom-[-10%] lg:bottom-[0%] bottom-[40%] w-full h-auto max-w-none z-[1]" />
                 <OurMission />
                 <OurMissionSection />
                 <TheJourneyMerg />
             </div>
-
         </div>
-
     )
 }
 
