db915023a4b19959e87742f980024357a93a5a20
NNikusha <116583730+NNikusha@users.noreply.github.com>
2025-04-15 01:29:09 +0400

    remove use client

--- src/app/components/atoms/ApartmentInformationCard/ApartmentInformationCard.tsx
+++ src/app/components/atoms/ApartmentInformationCard/ApartmentInformationCard.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from 'next/image';
 import React from 'react';
 import FloorIcon from '@/app/assets/FloorIcon.svg';
@@ -37,72 +35,72 @@ const ApartmentInformationCard = () => {
         </div>
       </div>
 
-        <div className="flex flex-col gap-[24px] w-full pb-[32px] border-b border-[#D3D3D3] border-b-[1px] mt-[32px]">
-            {/* FLOOR */}
-            <div className="flex gap-[8px] items-center justify-between lg:justify-start">
-                <div className="flex gap-[8px]">
-                    <Image src={FloorIcon} alt="Floor Icon" width={24} height={24} />
-                    FLOOR:
-                </div>
-                <div>{flatData.find((item) => item.key === "floor")?.value}</div>
-            </div>
-
-            {/* APARTMENT */}
-            <div className="flex gap-[8px] items-center justify-between lg:justify-start">
-                <div className="flex gap-[8px]">
-                    <Image src={ApartmentIcon} alt="Apartment Icon" width={24} height={24} />
-                    APARTMENT:
-                </div>
-                <div>{flatData.find((item) => item.key === "apartment")?.value}</div>
-            </div>
-
-            {/* TYPE */}
-            <div className="flex gap-[8px] items-center justify-between lg:justify-start">
-                <div className="flex gap-[8px]">
-                    <Image src={TypeIcon} alt="Type Icon" width={24} height={24} />
-                    TYPE:
-                </div>
-                <div>{flatData.find((item) => item.key === "type")?.value}</div>
-            </div>
+      <div className="flex flex-col gap-[24px] w-full pb-[32px] border-b border-[#D3D3D3] border-b-[1px] mt-[32px]">
+        {/* FLOOR */}
+        <div className="flex gap-[8px] items-center justify-between lg:justify-start">
+          <div className="flex gap-[8px]">
+            <Image src={FloorIcon} alt="Floor Icon" width={24} height={24} />
+            FLOOR:
+          </div>
+          <div>{flatData.find((item) => item.key === "floor")?.value}</div>
         </div>
 
-        <div className="flex flex-col gap-[24px] w-full pb-[32px] border-b border-[#D3D3D3] border-b-[1px] mt-[32px]">
-            {/* LIVING SPACE */}
-            <div className="flex gap-[8px] items-center justify-between lg:justify-start">
-                <div className="flex gap-[8px]">
-                    <Image src={LivingRoomIcon} alt="Living Space Icon" width={24} height={24} />
-                    LIVINGSPACE:
-                </div>
-                <div>{flatData.find((item) => item.key === "livingSpace")?.value}</div>
-            </div>
-
-            {/* BEDROOM */}
-            <div className="flex gap-[8px] items-center justify-between lg:justify-start">
-                <div className="flex gap-[8px]">
-                    <Image src={BedroomIcon} alt="Bedroom Icon" width={24} height={24} />
-                    BEDROOM:
-                </div>
-                <div>{flatData.find((item) => item.key === "bedroom")?.value}</div>
-            </div>
-
-            {/* BATHROOM */}
-            <div className="flex gap-[8px] items-center justify-between lg:justify-start">
-                <div className="flex gap-[8px]">
-                    <Image src={BathroomIcon} alt="Bathroom Icon" width={24} height={24} />
-                    BATHROOM:
-                </div>
-                <div>{flatData.find((item) => item.key === "bathroom")?.value}</div>
-            </div>
-
-            {/* BALCONY */}
-            <div className="flex gap-[8px] items-center justify-between lg:justify-start">
-                <div className="flex gap-[8px]">
-                    <Image src={BalconyIcon} alt="Balcony Icon" width={24} height={24} />
-                    BALCONY:
-                </div>
-                <div>{flatData.find((item) => item.key === "balcony")?.value}</div>
-            </div>
+        {/* APARTMENT */}
+        <div className="flex gap-[8px] items-center justify-between lg:justify-start">
+          <div className="flex gap-[8px]">
+            <Image src={ApartmentIcon} alt="Apartment Icon" width={24} height={24} />
+            APARTMENT:
+          </div>
+          <div>{flatData.find((item) => item.key === "apartment")?.value}</div>
         </div>
+
+        {/* TYPE */}
+        <div className="flex gap-[8px] items-center justify-between lg:justify-start">
+          <div className="flex gap-[8px]">
+            <Image src={TypeIcon} alt="Type Icon" width={24} height={24} />
+            TYPE:
+          </div>
+          <div>{flatData.find((item) => item.key === "type")?.value}</div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-[24px] w-full pb-[32px] border-b border-[#D3D3D3] border-b-[1px] mt-[32px]">
+        {/* LIVING SPACE */}
+        <div className="flex gap-[8px] items-center justify-between lg:justify-start">
+          <div className="flex gap-[8px]">
+            <Image src={LivingRoomIcon} alt="Living Space Icon" width={24} height={24} />
+            LIVINGSPACE:
+          </div>
+          <div>{flatData.find((item) => item.key === "livingSpace")?.value}</div>
+        </div>
+
+        {/* BEDROOM */}
+        <div className="flex gap-[8px] items-center justify-between lg:justify-start">
+          <div className="flex gap-[8px]">
+            <Image src={BedroomIcon} alt="Bedroom Icon" width={24} height={24} />
+            BEDROOM:
+          </div>
+          <div>{flatData.find((item) => item.key === "bedroom")?.value}</div>
+        </div>
+
+        {/* BATHROOM */}
+        <div className="flex gap-[8px] items-center justify-between lg:justify-start">
+          <div className="flex gap-[8px]">
+            <Image src={BathroomIcon} alt="Bathroom Icon" width={24} height={24} />
+            BATHROOM:
+          </div>
+          <div>{flatData.find((item) => item.key === "bathroom")?.value}</div>
+        </div>
+
+        {/* BALCONY */}
+        <div className="flex gap-[8px] items-center justify-between lg:justify-start">
+          <div className="flex gap-[8px]">
+            <Image src={BalconyIcon} alt="Balcony Icon" width={24} height={24} />
+            BALCONY:
+          </div>
+          <div>{flatData.find((item) => item.key === "balcony")?.value}</div>
+        </div>
+      </div>
 
       <button className="flex gap-[16px] items-center mt-[32px] mb-[40px] cursor-pointer">
         <Image src={DownloadIcon} alt="Download Icon" width={24} height={24} />
--- src/app/components/atoms/FlatDetailPageFeatureCards/FlatDetailPageFeatureCards.tsx
+++ src/app/components/atoms/FlatDetailPageFeatureCards/FlatDetailPageFeatureCards.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import React from 'react'
 import { FeatureCard } from '../FeatureCard/FeatureCard'
 import HighlightLine from "@/app/assets/HighLightLine.svg";
