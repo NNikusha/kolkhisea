bb33d456d8d0e960ae8437cd5b8acf5f4153035a
NNikusha <116583730+NNikusha@users.noreply.github.com>
2025-04-25 02:14:36 +0400

    fix error vercel

--- src/app/[locale]/(pages)/flat-detail-page/[id]/page.tsx
+++ src/app/[locale]/(pages)/flat-detail-page/[id]/page.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import FlatDetailPage from '@/app/components/organisms/FlatDetailPage/FlatDetailPage'
-import { Metadata } from 'next'
+import React from 'react';
+import FlatDetailPage from '@/app/components/organisms/FlatDetailPage/FlatDetailPage';
+import { Metadata } from 'next';
+import { PageProps } from '../../../../../../.next/types/app/[locale]/page';
 
-type ImportedPageProps = import("/Users/nikushatavartkiladze/Desktop/kolkhisea/.next/types/app/[locale]/(pages)/flat-detail-page/[id]/page").PageProps;
-
-export async function generateMetadata({ params }: ImportedPageProps): Promise<Metadata> {
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
   const resolvedParams = params instanceof Promise ? await params : params;
   return {
     title: `Flat Detail - ${resolvedParams.id}`,
-  }
+  };
 }
 
-export default async function Page({ params }: ImportedPageProps) {
-
+export default async function Page({ params }: PageProps) {
   const resolvedParams = params instanceof Promise ? await params : params;
-  return <FlatDetailPage params={resolvedParams} />
+  return <FlatDetailPage params={resolvedParams} />;
 }
