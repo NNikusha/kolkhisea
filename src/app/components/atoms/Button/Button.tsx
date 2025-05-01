8c32037dbde062fd21fb268d7be0416f68fab6c9
NNikusha <116583730+NNikusha@users.noreply.github.com>
2025-05-01 12:57:06 +0400

    get in touch functionalty

--- src/app/components/atoms/Button/Button.tsx
+++ src/app/components/atoms/Button/Button.tsx
@@ -3,48 +3,51 @@ import Link from 'next/link';
 import DiagonalArrow from '@/app/assets/DiagonalArrow';
 
 interface ButtonProps {
-    text: string;
-    onClick?: () => void;
-    className?: string;
-    fullWidth?: boolean;
-    href?: string; 
+  text: string;
+  onClick?: () => void;
+  className?: string;
+  fullWidth?: boolean;
+  href?: string; 
+  disabled?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({
-    text,
-    onClick,
-    className = '',
-    fullWidth = false,
-    href
+  text,
+  onClick,
+  className = '',
+  fullWidth = false,
+  href,
+  disabled, 
 }) => {
-    const buttonClasses = `relative cursor-pointer bg-[#CB684D] text-white py-4 px-6 rounded-[16px] flex items-center gap-2 transition-transform duration-300 group ${fullWidth ? 'w-full justify-center' : ''} ${className}`;
+  const buttonClasses = `relative cursor-pointer bg-[#CB684D] text-white py-4 px-6 rounded-[16px] flex items-center gap-2 transition-transform duration-300 group ${fullWidth ? 'w-full justify-center' : ''} ${className}`;
 
-    const content = (
-        <>
-            <span className="relative z-10">{text}</span>
-            <div className='relative z-10 flex w-[15px] h-[15px]'>
-                <DiagonalArrow />
-            </div>
-            <div className="absolute inset-0 bg-[radial-gradient(30%_50%_at_50%_90%,rgba(255,255,255,0.3)_0%,rgba(255,255,255,0)_100%)] opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none"></div>
-        </>
-    );
+  const content = (
+    <>
+      <span className="relative z-10">{text}</span>
+      <div className='relative z-10 flex w-[15px] h-[15px]'>
+        <DiagonalArrow />
+      </div>
+      <div className="absolute inset-0 bg-[radial-gradient(30%_50%_at_50%_90%,rgba(255,255,255,0.3)_0%,rgba(255,255,255,0)_100%)] opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none"></div>
+    </>
+  );
 
-    if (href) {
-        return (
-            <Link href={href} className={buttonClasses} onClick={onClick}>
-                {content}
-            </Link>
-        );
-    }
-    
+  if (href) {
     return (
-        <button
-            onClick={onClick}
-            className={buttonClasses}
-        >
-            {content}
-        </button>
+      <Link href={href} className={buttonClasses} onClick={onClick}>
+        {content}
+      </Link>
     );
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      className={buttonClasses}
+      disabled={disabled} 
+    >
+      {content}
+    </button>
+  );
 };
 
 export default Button;
--- src/app/components/atoms/InputField/InputField.tsx
+++ src/app/components/atoms/InputField/InputField.tsx
@@ -1,38 +1,47 @@
+'use client'
+import React from 'react'
+
 type InputFieldProps = {
-    label: string
-    id: string
-    type?: string
-    placeholder?: string
-    onInput?: (e: React.FormEvent<HTMLInputElement>) => void
-    inputMode?: React.HTMLAttributes<HTMLInputElement>['inputMode']
-    className?: string
-  }
-  
-  const InputField = ({
-    label,
-    id,
-    type = 'text',
-    placeholder,
-    onInput,
-    inputMode,
-    className = '',
-  }: InputFieldProps) => (
-    <div className="pb-[24px]">
-      <label htmlFor={id} className="block font-normal text-[#1C1C1E] pb-[8px] sm:text-[16px] text-[14px]">
-        {label}
-      </label>
-      <div className="w-full border border-[#E3E3E3] rounded-[8px] px-4 py-3 focus-within:border-[#1C1C1E] transition-colors duration-200">
-        <input
-          type={type}
-          id={id}
-          name={id}
-          inputMode={inputMode}
-          onInput={onInput}
-          className={`w-full border-none outline-none text-[#1C1C1E] placeholder-[#D3D3D3] ${className}`}
-          placeholder={placeholder}
-        />
-      </div>
+  label: string
+  id: string
+  type?: string
+  placeholder?: string
+  onInput?: (e: React.FormEvent<HTMLInputElement>) => void
+  inputMode?: React.HTMLAttributes<HTMLInputElement>['inputMode']
+  className?: string
+  value?: string
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const InputField = ({
+  label,
+  id,
+  type = 'text',
+  placeholder,
+  onInput,
+  inputMode,
+  className = '',
+  value,
+  onChange,
+}: InputFieldProps) => (
+  <div className="pb-[24px]">
+    <label htmlFor={id} className="block font-normal text-[#1C1C1E] pb-[8px] sm:text-[16px] text-[14px]">
+      {label}
+    </label>
+    <div className="w-full border border-[#E3E3E3] rounded-[8px] px-4 py-3 focus-within:border-[#1C1C1E] transition-colors duration-200">
+      <input
+        type={type}
+        id={id}
+        name={id}
+        inputMode={inputMode}
+        onInput={onInput}
+        value={value}
+        onChange={onChange}
+        className={`w-full border-none outline-none text-[#1C1C1E] placeholder-[#D3D3D3] ${className}`}
+        placeholder={placeholder}
+      />
     </div>
-  )
-  
-  export default InputField
+  </div>
+)
+
+export default InputField
