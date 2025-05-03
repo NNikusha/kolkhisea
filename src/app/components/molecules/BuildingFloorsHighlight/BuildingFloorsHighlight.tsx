9ca6b9911c74b99e1357ce60846822e1766060c7
JamesGlik <sadoiani9900@gmail.com>
2025-05-03 13:49:24 +0400

    fixed build error

--- src/app/components/molecules/BuildingFloorsHighlight/BuildingFloorsHighlight.tsx
+++ src/app/components/molecules/BuildingFloorsHighlight/BuildingFloorsHighlight.tsx
@@ -1,7 +1,6 @@
 "use client"
 import React, { useState, useRef } from 'react';
 import FullscreenApartmentModal from '../ApartmentSelectionModal/ApartmentSelectionModal';
-import ModalPortal from '../../atoms/ModalPortal/ModalPortal';
 
 interface ShapeData {
   points: number[][];
