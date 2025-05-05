5f607ba6662f4958ed2d31298443bf2f7dec3471
NNikusha <116583730+NNikusha@users.noreply.github.com>
2025-05-05 13:24:55 +0400

    Update WhoWeAre.tsx

--- src/app/components/atoms/WhoWeAre/WhoWeAre.tsx
+++ src/app/components/atoms/WhoWeAre/WhoWeAre.tsx
@@ -22,7 +22,7 @@ const WhoWeAre = ({
     const t = useTranslations('Language');
 
     return (
-        <div className='container px-[16px] lg:px-[108px] mx-auto w-full bg-[#F3F6FB] mt-[calc(270px+40vw)] sm:mt-[700px] lg:mt-[710px] xl:mt-[470px] 2xl:mt-[560px]'>
+        <div className='container px-[16px] lg:px-[108px] mx-auto w-full bg-[#F3F6FB] mt-[calc(270px+40vw)] sm:mt-[800px] lg:mt-[810px] xl:mt-[570px] 2xl:mt-[660px]'>
             <div className='flex flex-col pb-[48px] w-full'>
                 <div className='w-full flex items-center justify-center'>
                     <h1 className='text-[#1C1C1E] text-[32px] md:text-[64px] font-normal pb-[24px] uppercase'>
