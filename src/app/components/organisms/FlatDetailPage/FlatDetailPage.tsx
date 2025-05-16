10e3a7449a980f291756277c2cd2353cc82e7925
JamesGlik <sadoiani9900@gmail.com>
2025-05-16 17:29:54 +0400

    removed extra lines

--- src/app/components/organisms/FlatDetailPage/FlatDetailPage.tsx
+++ src/app/components/organisms/FlatDetailPage/FlatDetailPage.tsx
@@ -4,8 +4,6 @@ import SuggestApartment from '../../atoms/SuggestApartment/SuggestApartment';
 import FlatDetailPageFeatureCards from '../../atoms/FlatDetailPageFeatureCards/FlatDetailPageFeatureCards';
 import ApartmentInformation from '../../molecules/ApartmentInformation/ApartmentInformation';
 import ApartmentInformationCard from '../../atoms/ApartmentInformationCard/ApartmentInformationCard';
-import BackGroundLine6 from '@/app/assets/BackGroundLine6';
-import BackGroundLine7 from '@/app/assets/BackGroundLine7';
 import { Locale } from '@/app/types/type';
 import { fetchProjectAbout, fetchFlatById } from '@/app/hooks/axios';
 import { getLocale } from 'next-intl/server';
@@ -32,8 +30,8 @@ const FlatDetailPage = async ({ params }: FlatDetailPageProps) => {
         <ApartmentInformation />
         <div className='w-full bg-white rounded-t-[56px] mt-[32px] lg:mt-[40px] pt-[24px] lg:pt-[60px] pb-[72px] md:pb-[168px] relative'>
           <div className="absolute inset-0 w-full h-full overflow-hidden pointer-events-none">
-            <BackGroundLine6 className="absolute top-[50%] lg:top-[50%] w-full h-auto max-w-none" />
-            <BackGroundLine7 className="absolute top-[40%] lg:top-[20%] w-full h-auto max-w-none" />
+            {/* <BackGroundLine6 className="absolute top-[50%] lg:top-[50%] w-full h-auto max-w-none" />
+            <BackGroundLine7 className="absolute top-[40%] lg:top-[20%] w-full h-auto max-w-none" /> */}
           </div>
           <div className="container mx-auto">
             <div className='flex justify-center items-center lg:hidden'>
