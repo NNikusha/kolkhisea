a78973d105a9f9c2891521b5b117ef22329bbbbb
NNikusha <116583730+NNikusha@users.noreply.github.com>
2025-10-06 17:32:38 +0400

    Update ClientAboutUs.tsx

--- src/app/components/organisms/ClientAboutUs/ClientAboutUs.tsx
+++ src/app/components/organisms/ClientAboutUs/ClientAboutUs.tsx
@@ -5,7 +5,6 @@ import AboutUsSection from "../../molecules/AboutUsSection/AboutUsSection";
 import OurMissionSection from "../../molecules/OurMissionSection/OurMissionSection";
 import WhoWeAre from "../../atoms/WhoWeAre/WhoWeAre";
 import TheJourneyMerg from "../../molecules/TheJourneyMerg/TheJourneyMerg";
-import OurMission from "../../atoms/OurMission/OurMission";
 import GetInTouchSection from "../../molecules/GetInTouchSection/GetInTouchSection";
 import Flower1AboutUs from "@/app/assets/Flower1AboutUs";
 import Image from "next/image";
